5e20113c87f1c3e1018bfe11113fe25c60b032be
Janus Vollmer <janus.vollmer@gmail.com>
2021-08-09 19:55:48 +0200

    refactored the code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,24 +5,22 @@ class GamesController < ApplicationController
     @letters
   end
 
+  def check(word)
+    require 'open-uri'
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    JSON.parse(open(url).read)['found']
+  end
+
   def score
-    require 'net/http'
-    require 'json'
-
-    base_url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
-    uri = URI(base_url)
-    response = Net::HTTP.get(uri)
-    check = JSON.parse(response)['found']
-
-    solution = params[:word].upcase.split('')
+    word = params[:word]
+    solution = word.upcase.split('')
     @letters = params[:letters].split('')
     if (solution & @letters) != solution
-      @prompt = "Sorry, #{params[:word]} can not be built with #{@letters}!"
-    elsif check
-      @prompt = "Congratulations, you won. Your score is #{params[:word].length * 10} points!"
+      @prompt = "Sorry, #{word} can not be built with #{@letters}!"
+    elsif check(word)
+      @prompt = "Congratulations, you won. Your score is #{word.length * 10} points!"
     else
       @prompt = "Sorry, #{params[:word]} is not in the dictionary"
     end
   end
-
 end
